94578fe8689d720c3e3ce167a93fb6fa600fb630
rindera09 <rindera09@gmail.com>
2017-01-20 14:53:19 +0800

    2017.01 update

--- foxrenderfarm.py
+++ foxrenderfarm.py
@@ -40,20 +40,16 @@ class Fox(Api):
     root = os.path.dirname(os.path.abspath(__file__))
     ascp_exe = os.path.join(root, "aspera", "ascp.exe")
 
-    def __init__(self, render_server, account, access_key,
-                 aspera_server, aspera_password, language="en"):
+    def __init__(self, render_server, account, access_key, language="en"):
         Api.__init__(self, render_server)
         self.data = {"head": {"access_key": access_key,
                               "account": account,
                               "msg_locale": language,
                               "action": ""},
                      "body": {}}
-
         self.login()
-        self.aspera_server = aspera_server
-        self.aspera_upload = self.account_id + "_upload"
-        self.aspera_download = self.account_id + "_download"
-        self.aspera_password = aspera_password
+        self.rayvision_exe = os.path.join("rayvision", "rayvision_transmitter")
+        self._init_upload_download_config()
 
     def login(self):
         result = self.get_users()
@@ -61,6 +57,18 @@ class Fox(Api):
             self.account_id = result[0]["id"]
         else:
             raise Exception("account or access_key is not valid.")
+
+    def _init_upload_download_config(self):
+        user_info = self.get_users()[0]
+        self.upload_id = user_info["upload_id"]
+        self.download_id = user_info["download_id"]
+        self.transports = user_info["transports"]
+
+        if self.transports:
+            self.engine_type = self.transports[0]["engine"]
+            self.server_name = self.transports[0]["server"]
+            self.server_ip = self.transports[0]["ip"]
+            self.server_port = self.transports[0]["port"]
 
     def submit_task(self, **kwargs):
         data = copy.deepcopy(self.data)
@@ -84,7 +92,12 @@ class Fox(Api):
             raise Exception("Project <%s> doesn't exists." % (kwargs["project_name"]))
 
         plugins = project[0]["plugins"]
-        if not plugins:
+        no_plugin = True
+        for i in plugins:
+            if i:
+                no_plugin = False
+                break
+        if no_plugin:
             raise Exception("Project <%s> doesn't have any plugin settings." % (kwargs["project_name"]))
 
         default_plugin = [i for i in plugins
@@ -156,50 +169,54 @@ class Fox(Api):
         else:
             return []
 
-    def upload(self, path_list, skip_same=1, user=None, password=None):
-        user = user if user else self.aspera_upload
-        os.environ["ASPERA_SCP_PASS"] = password if password else self.aspera_password
-        overwrite = "older" if skip_same else "always"
+    def upload(self, local_path_list, server_path='/', **kwargs):
 
+        transmit_type = "upload_files"
         result = {}
-        for i in set(path_list):
+        for i in set(local_path_list):
             if os.path.exists(i):
-                server_path = os.path.dirname(i).replace(":", "")
-                cmd = "echo y | \"%s\" -P 33001 -O 33001 -d -p -l 1000000 " \
-                      "--overwrite=%s \"%s\" %s@%s:/%s" % (self.ascp_exe,
-                                                           overwrite,
-                                                           i,
-                                                           user,
-                                                           self.aspera_server,
-                                                           server_path)
+                local_path = i
+                cmd = "echo y | %s %s %s %s %s %s %s %s %s %s" % (self.rayvision_exe,
+                                                                  self.engine_type,
+                                                                  self.server_name,
+                                                                  self.server_ip,
+                                                                  self.server_port,
+                                                                  self.upload_id,
+                                                                  self.account_id,
+                                                                  transmit_type,
+                                                                  local_path,
+                                                                  server_path)
                 print cmd
+                result[i] = True
                 sys.stdout.flush()
                 result[i] = os.system(cmd)
             else:
-                result[i] = -1
-
+                result[i] = False
         return result
 
-    def download(self, task_id, local_path, skip_same=1, user=None, password=None):
-        user = user if user else self.aspera_download
-        os.environ["ASPERA_SCP_PASS"] = password if password else self.aspera_password
-        overwrite = "older" if skip_same else "always"
+    def download(self, task_id, local_path, **kwargs):
 
+        transmit_type = "download_files"
         task = self.get_tasks(task_id)
+
         if task:
-            server_path = "%s_%s" % (task_id, os.path.splitext(os.path.basename(task[0]["scene_name"]))[0])
-            cmd = "echo y | \"%s\" -P 33001 -O 33001 -d -p -l 1000000 " \
-                  "--overwrite=%s %s@%s:/%s \"%s\"" % (self.ascp_exe,
-                                                       overwrite,
-                                                       user,
-                                                       self.aspera_server,
-                                                       server_path,
-                                                       local_path)
+            input_scene_path = task[0]["input_scene_path"]
+            server_path = "%s_%s" % (task_id, os.path.splitext(os.path.basename(input_scene_path))[0].strip())
+            cmd = "echo y | %s %s %s %s %s %s %s %s %s %s" % (self.rayvision_exe,
+                                                              self.engine_type,
+                                                              self.server_name,
+                                                              self.server_ip,
+                                                              self.server_port,
+                                                              self.download_id,
+                                                              self.account_id,
+                                                              transmit_type,
+                                                              local_path,
+                                                              server_path)
             print cmd
             sys.stdout.flush()
             return os.system(cmd)
         else:
-            return -1
+            return False
 
     def get_server_files(self):
         ''
@@ -207,11 +224,10 @@ class Fox(Api):
     def delete_server_files(self):
         ''
 
-
     """ NO 7.2.3
-    :param project_name: the name of the project you want to create
-    :param kwargs: can be used to pass more arguments, not necessary
-                   including project_path, render_os, remark, sub_account
+        :param project_name: the name of the project you want to create
+        :param kwargs:  can be used to pass more arguments, not necessary
+                        including project_path, render_os, remark, sub_account
 
     """
     def create_project(self, project_name, **kwargs):
@@ -233,14 +249,12 @@ class Fox(Api):
             self._message_output("ERROR", result["head"]["error_message"])
             return False
 
-
     def _message_output(self, msg_type=None, msg=None):
         print "[{0}]: {1}".format(msg_type, msg)
 
-
     """ NO: 7.2.2  Query plugins
-        :param kwargs: can be used to pass more arguments, not necessary
-                   including cg_soft_name, plugin_name
+        :param kwargs:  can be used to pass more arguments, not necessary
+                        including cg_soft_name, plugin_name
         Here some examples::
                 get_plugin()
                 get_plugin(cg_soft_name="3ds Max 2010")
@@ -250,7 +264,7 @@ class Fox(Api):
         data = copy.deepcopy(self.data)
         data["head"]["action"] = "query_plugin"
         for key, value in kwargs.items():
-            data["body"][key] = value       
+            data["body"][key] = value
 
         result = self.post(data=data)
         if result["head"]["result"] == "0":
@@ -260,13 +274,203 @@ class Fox(Api):
             self._message_output("ERROR", result["head"]["error_message"])
             return False
 
-
     def _save_list2file(self, list_data, file_name, remark="\n"):
         basedir = os.path.abspath(os.path.dirname(__file__))
         save_path = os.path.join(basedir, file_name)
-        with open(save_path,"a+") as f:
+        with open(save_path, "a+") as f:
             if remark:
                 f.write(remark)
             for line in list_data:
                 f.write(str(line) + "\n")
         print "[INFO]:" + save_path + " has saved."
+
+    """ NO 7.2.4 Add config for project
+        :param project_id:  the id of the existed project you choose
+        :param cg_soft_name:  the software you use
+        :param plugin_name:  the plugin you use
+        :param is_default:  make it as default setting
+        :param kwargs: can  be used to pass more arguments, not necessary
+
+    """
+    def add_project_config(self, project_id, cg_soft_name, plugin_name=None,
+                           is_default=0, **kwargs):
+        data = copy.deepcopy(self.data)
+        data["head"]["action"] = "operate_project"
+        data["body"]["operate_type"] = 0
+
+        data["body"]["project_id"] = int(project_id)
+        data["body"]["cg_soft_name"] = cg_soft_name
+        if plugin_name:
+            data["body"]["plugin_name"] = plugin_name
+        data["body"]["is_default"] = is_default
+        for key, value in kwargs.items():
+            data["body"][key] = value
+
+        result = self.post(data=data)
+        if result["head"]["result"] == "0":
+            return True
+        else:
+            self._message_output("ERROR", result["head"]["error_message"])
+            return False
+
+    """ NO 7.2.4 Delete config for project
+        :param project_id: the id of the existed project you choose
+        :param config_id: the id of configuration you want to delete
+                          if not pass this argument it will delete all
+                          you can use "get_project_info" to get config_id
+        :param kwargs: can be used to pass more arguments, not necessary
+
+    """
+    def delete_project_config(self, project_id, config_id=None, **kwargs):
+        data = copy.deepcopy(self.data)
+        data["head"]["action"] = "operate_project"
+        data["body"]["operate_type"] = 2
+
+        data["body"]["project_id"] = int(project_id)
+        if config_id:
+            data["body"]["config_id"] = int(config_id)
+        for key, value in kwargs.items():
+            data["body"][key] = value
+
+        result = self.post(data=data)
+        if result["head"]["result"] == "0":
+            self._message_output("INFO", "configuration delete")
+            return True
+        else:
+            self._message_output("ERROR", result["head"]["error_message"])
+            return False
+
+    """ NO 7.2.4 Modify config for project
+        :param project_id:  the id of the existed project you choose
+        :param config_id:  the id of configuration you want to delete
+                           if not pass this argument it will delete all
+                           you can use "get_projects" to get config_id
+        :param cg_soft_name:  the software you use
+        :param plugin_name:  the plugin you use
+        :param is_default:  make it as default setting, just one default allowed
+        :param kwargs: can  be used to pass more arguments, not necessary
+
+    """
+    def modify_project_config(self, project_id, config_id, cg_soft_name,
+                              plugin_name=None, is_default=None, **kwargs):
+        data = copy.deepcopy(self.data)
+        data["head"]["action"] = "operate_project"
+        data["body"]["operate_type"] = 1
+
+        data["body"]["project_id"] = int(project_id)
+        data["body"]["config_id"] = int(config_id)
+        data["body"]["cg_soft_name"] = cg_soft_name
+        if plugin_name is not None:
+            data["body"]["plugin_name"] = plugin_name
+        if is_default:
+            data["body"]["is_default"] = int(is_default)
+        for key, value in kwargs.items():
+            data["body"][key] = value
+
+        result = self.post(data=data)
+        if result["head"]["result"] == "0":
+            self._message_output("INFO", "modify the configuration")
+            return True
+        else:
+            self._message_output("ERROR", result["head"]["error_message"])
+            return False
+
+    """ NO 7.1.3 Restart the tasks
+        :param task_id:  the tasks you what to restart
+        :param restart_type:  0 -- restart the failed frames
+                              1 -- restart the frames that give up
+                              2 -- restart the finished frames
+                              3 -- restart the start frames
+                              4 -- restart the waiting frames
+        Here some example::  restart_tasks("123", "0")
+                             restart_tasks(["123", "456"], "3")
+    """
+    def restart_tasks(self, task_id, restart_type="0"):
+        data = copy.deepcopy(self.data)
+        data["head"]["action"] = "operate_task"
+        data["body"]["operate_order"] = "1"
+        data["body"]["restart_type"] = str(restart_type)
+        if isinstance(task_id, list) and len(task_id) > 1:
+            task_id = ''.join([str(id) + ',' for id in task_id[:-1]]) + str(task_id[-1])
+        data["body"]["task_id"] = str(task_id)
+
+        result = self.post(data=data)
+        if result["head"]["result"] == "0":
+            self._message_output("INFO", "task {0} restart.".format(task_id))
+            return True
+        else:
+            self._message_output("ERROR", result["head"]["error_message"])
+            return False
+
+    """ NO 7.1.3 Stop the tasks
+        :param task_id:  the tasks you what to pause
+
+        Here some example::  stop_tasks(123)
+                             stop_tasks("123")
+                             stop_tasks(["123", "456"])
+                             stop_tasks([123, 456])
+    """
+    def stop_tasks(self, task_id):
+        data = copy.deepcopy(self.data)
+        data["head"]["action"] = "operate_task"
+        data["body"]["operate_order"] = "0"
+        if isinstance(task_id, list) and len(task_id) > 1:
+            task_id = ''.join(map(lambda id: str(id) + ",", task_id[:-1])) + str(task_id[-1])
+        data["body"]["task_id"] = str(task_id)
+
+        result = self.post(data=data)
+        if result["head"]["result"] == "0":
+            self._message_output("INFO", "task {0} paused.".format(task_id))
+            return True
+        else:
+            self._message_output("ERROR", result["head"]["error_message"])
+            return False
+
+    """ NO 7.1.3 Delete the tasks
+        :param task_id:  the tasks you what to delete
+
+        Here some example::  delete_tasks(123)
+                             delete_tasks("123")
+                             delete_tasks(["123", "456"])
+                             delete_tasks([123, 456])
+    """
+    def delete_tasks(self, task_id):
+        data = copy.deepcopy(self.data)
+        data["head"]["action"] = "operate_task"
+        data["body"]["operate_order"] = "2"
+        if isinstance(task_id, list) and len(task_id) > 1:
+            task_id = ''.join(map(lambda id: str(id) + ",", task_id[:-1])) + str(task_id[-1])
+        data["body"]["task_id"] = str(task_id)
+
+        result = self.post(data=data)
+        if result["head"]["result"] == "0":
+            self._message_output("INFO", "task {0} deleted.".format(task_id))
+            return True
+        else:
+            self._message_output("ERROR", result["head"]["error_message"])
+            return False
+
+    """ Get the plugins of the project
+        :param project_name:  the name of the project
+    """
+    def get_project_plugins_config(self, project_name):
+        data = copy.deepcopy(self.data)
+        data["head"]["action"] = "query_project"
+
+        if not project_name:
+            self._message_output("WARNING", "Mising project name")
+            return []
+
+        data["body"]["project_name"] = project_name
+
+        result = self.post(data)
+        plugins = []
+        if result["body"]["data"]:
+            plugins = result["body"]["data"][0]["plugins"]
+
+        if result["head"]["result"] == "0":
+            self._message_output("INFO", "Query plugins config id:")
+            return plugins
+        else:
+            self._message_output("WARNING", result["head"]["error_message"])
+            return []
